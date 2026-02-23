c53e317b8bc41a55530109d027f32784536c113e
Henrik Finsberg <henriknf@simula.no>
2026-02-23 14:59:44 +0100

    Add MRI toolkit and textual-image

---
+++ spack_repo/scientificcomputing/packages/py_textual_image/package.py
@@ -0,0 +1,50 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install py-mri-toolkit
+#
+# You can edit this file again by typing:
+#
+#     spack edit py-mri-toolkit
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyTextualImage(PythonPackage):
+    """Render images via Kitty's Terminal Graphics Protocol with Rich and Textual"""
+
+    homepage = "https://github.com/lnqs/textual-image"
+    pypi = "textual-image/textual_image-0.8.5.tar.gz"
+
+    license("LGPL-3.0-or-later", checked_by="finsberg")
+
+    maintainers("finsberg")
+
+    version(
+        "0.8.5",
+        sha256="43d4c0026a4f21fa255f41eac7b0fc1f7410a4c7bc9bf95b908bec901b0a8c3a",
+    )
+
+    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
+    # or need to change the dependency type. Generic python/pip/wheel dependencies are
+    # added implicity by the PythonPackage base class.
+    depends_on("python@3.10:", type=("build", "run"))
+
+    depends_on("py-setuptools", type="build")
+    depends_on("py-wheel", type="build")
+    depends_on("py-pillow", type="run")
+    depends_on("py-rich", type="run")
