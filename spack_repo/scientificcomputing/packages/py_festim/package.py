7388af3f0ac4b7543dee05dc2eb44a26bdfadd29
Jørgen S. Dokken <dokken@simula.no>
2026-02-26 12:51:02 +0100

    More formatting

--- spack_repo/scientificcomputing/packages/py_festim/package.py
+++ spack_repo/scientificcomputing/packages/py_festim/package.py
@@ -12,9 +12,7 @@ class PyFestim(PythonPackage):
     It simulates the diffusion and trapping of hydrogen, coupled to heat transfer with FEniCS."""
 
     homepage = "https://festim.readthedocs.io/en/latest/"
-    url = (
-        "https://github.com/festim-dev/FESTIM/archive/refs/tags/v2.0-beta.2post1.tar.gz"
-    )
+    url = "https://github.com/festim-dev/FESTIM/archive/refs/tags/v2.0-beta.2post1.tar.gz"
     git = "https://github.com/festim-dev/FESTIM.git"
 
     maintainers("jorgensd")
@@ -37,12 +35,12 @@ class PyFestim(PythonPackage):
 
     depends_on("py-fenics-dolfinx@0.10+petsc4py", when="@2.0-beta.2post1", type="run")
     depends_on("py-fenics-dolfinx@main+petsc4py", when="@main", type="run")
-   
+
     depends_on("fenics-dolfinx+adios2", type="run")
 
     depends_on(
         "hdf5@1.12:", type="build"
-        )  # NOTE: Remove when https://github.com/spack/spack-packages/issues/3566 is resolved
+    )  # NOTE: Remove when https://github.com/spack/spack-packages/issues/3566 is resolved
     depends_on("petsc+mumps", type="run")
    
     depends_on("py-numpy", type="run")
