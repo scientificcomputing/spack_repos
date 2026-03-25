dc3f955c27ac0e77758f7abcf5de4ecd2d2d5d57
Henrik Finsberg <henriknf@simula.no>
2026-03-25 08:48:12 +0100

    Make gmsh dependency explicit

--- spack_repo/scientificcomputing/packages/py_cardiac_geometries_core/package.py
+++ spack_repo/scientificcomputing/packages/py_cardiac_geometries_core/package.py
@@ -48,5 +48,6 @@ class PyCardiacGeometriesCore(PythonPackage):
 
     # Core dependencies
     depends_on("py-numpy", type=("build", "run"))
-    depends_on("py-gmsh@4.13.1 ^gmsh@4.13.1", type=("build", "run"))
+    depends_on("py-gmsh@:4.13.1", type=("build", "run"))
+    depends_on("gmsh@:4.13.1", type=("build", "run"))
     depends_on("py-rich-click", type=("build", "run"))
