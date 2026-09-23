f06af2807982133fd4dd5e2fd563751aebfe63ff
Henrik Finsberg <henrikfinsberg@hotmail.com>
2026-09-23 16:28:23 +0200

    Lint

--- spack_repo/scientificcomputing/packages/dolfinx_mpc/package.py
+++ spack_repo/scientificcomputing/packages/dolfinx_mpc/package.py
@@ -31,7 +31,9 @@ class DolfinxMpc(CMakePackage):
     version(
         "0.11.0.post0", sha256="2874423026f74c03ddd26ab949fd3705bc6bca066195ff79c6661e90f8839df8"
     )
-    version("0.12.0.dev0", sha256="a18ac6a041cdc3af67977a1a71d33459863fe609167a51141a893352cf1d0480")
+    version(
+        "0.12.0.dev0", sha256="a18ac6a041cdc3af67977a1a71d33459863fe609167a51141a893352cf1d0480"
+    )
 
     # HDF5 dependency requires C in CMake
     depends_on("c", type="build")
