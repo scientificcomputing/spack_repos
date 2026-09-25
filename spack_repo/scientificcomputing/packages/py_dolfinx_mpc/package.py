71e217312166b89ea33efb7cd6bfceebcd04f30a
Henrik Finsberg <henrikfinsberg@hotmail.com>
2026-09-25 08:45:41 +0200

    Add py_crossbridge

--- spack_repo/scientificcomputing/packages/py_dolfinx_mpc/package.py
+++ spack_repo/scientificcomputing/packages/py_dolfinx_mpc/package.py
@@ -34,7 +34,9 @@ class PyDolfinxMpc(PythonPackage):
     version(
         "0.12.0.dev0", sha256="a18ac6a041cdc3af67977a1a71d33459863fe609167a51141a893352cf1d0480"
     )
-    version("0.12.0.dev0", sha256="a18ac6a041cdc3af67977a1a71d33459863fe609167a51141a893352cf1d0480")
+    version(
+        "0.12.0.dev0", sha256="a18ac6a041cdc3af67977a1a71d33459863fe609167a51141a893352cf1d0480"
+    )
 
     variant("numba", default=False, description="numba support")
 
